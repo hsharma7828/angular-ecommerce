720f609a691866e87b0b3aca5ee2d9b71f1e33c8
Himanshu <hsharma114@gmail.com>
2024-04-29 10:03:47 +0530

    v1:master Search implemtation and symentic fixes

--- src/app/components/product-list/product-list.component.ts
+++ src/app/components/product-list/product-list.component.ts
@@ -1,27 +1,56 @@
 import { Component, OnInit } from '@angular/core';
 import { ProductService } from '../../services/product.service';
 import { Product } from '../../common/product';
+import { ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-product-list',
-  // templateUrl: './product-list.component.html',
-  // templateUrl: './product-list-table.component.html',
   templateUrl: './product-list-grid.component.html',
   styleUrl: './product-list.component.css'
 })
 export class ProductListComponent implements OnInit {
 
   products: Product[] = [];
+  currentCategoryId: number = 1;
+  searchMode: boolean = false;
 
   constructor(
-    private productService: ProductService
+    private productService: ProductService,
+    private route: ActivatedRoute
   ) {}
 
   ngOnInit(): void {
-    this.listProducts();
+    this.route.paramMap.subscribe(param => {
+      this.listProducts();
+    })
   }
   listProducts() {
-    this.productService.getProductList().subscribe( data => {
+    this.searchMode = this.route.snapshot.paramMap.has('keyword');
+    if(this.searchMode) {
+      this.handleSearchProducts();
+    } else {
+      this.handleListProduct();
+    }
+  }
+  handleSearchProducts() {
+    const theKeyWord: string = this.route.snapshot.paramMap.get('keyword')!;
+
+    // now search for the products using the keyword
+    this.productService.serachProducts(theKeyWord).subscribe(data => {
+        this.products = data;
+      }
+    );
+  }
+
+  handleListProduct() {
+    /*check if "id" parameter avaialble*/
+    const hasCategoryId: boolean = this.route.snapshot.paramMap.has('id');
+
+    /*get the "id" param string. convert string to number using "+" sysmbol 
+    else use "1" as default*/
+    this.currentCategoryId = hasCategoryId ? +this.route.snapshot.paramMap.get('id')! : 1;
+
+    this.productService.getProductList(this.currentCategoryId).subscribe(data => {
       this.products = data;
     });
   }
--- src/app/services/product.service.ts
+++ src/app/services/product.service.ts
@@ -2,26 +2,48 @@ import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable, map } from 'rxjs';
 import { Product } from '../common/product';
+import { ProductCategory } from '../common/product-category';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ProductService {
 
-  private baseUrl = "http://localhost:8080/api/products?size=100";
+  private productUrl = "http://localhost:8080/api/products";
+  private categoryUrl = "http://localhost:8080/api/product-category";
 
-  constructor( private httpClient: HttpClient) { }
+  constructor(private httpClient: HttpClient) { }
 
+  getProductList(theCategoryId: number): Observable<Product[]> {
+    /* need to build URL based on category id ...*/
+    const searchUrl = `${this.productUrl}/search/findByCategoryId?id=${theCategoryId}`;
+    return this.getProducts(searchUrl);
+  }
+  getProductCategories(): Observable<ProductCategory[]> {
+    return this.httpClient.get<GetResponseProductCategory>(this.categoryUrl).pipe(
+      map(response => response._embedded.productCategory)
+    );
+  }
+  serachProducts(theKeyWord: string): Observable<Product[]> {
+    /* need to build URL based on the keyword ...*/
+    const searchUrl = `${this.productUrl}/search/findByNameContaining?name=${theKeyWord}`;
+    return this.getProducts(searchUrl);
+  }
 
-  getProductList(): Observable<Product[]> {
-    return this.httpClient.get<GetResponse>(this.baseUrl).pipe(
+  private getProducts(searchUrl: string): Observable<Product[]> {
+    return this.httpClient.get<GetResponseProduct>(searchUrl).pipe(
       map(response => response._embedded.products)
     );
   }
 }
 
-interface GetResponse {
+interface GetResponseProduct {
   _embedded: {
     products: Product[];
+  }
+}
+interface GetResponseProductCategory {
+  _embedded: {
+    productCategory: ProductCategory[];
   }
 }
